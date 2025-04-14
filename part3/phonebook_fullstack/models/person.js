03baac263af12e12118b9177e2f58294192c7ed4
niko.roytio <niko.roytio@hurja.fi>
2025-04-14 18:36:57 +0300

    finished fullstack phonebook before deploying to fly.io

--- part3/phonebook_fullstack/models/person.js
+++ part3/phonebook_fullstack/models/person.js
@@ -22,7 +22,13 @@ const personSchema = new mongoose.Schema({
   number: {
     type: String,
     minLength: 8,
-    required: true
+    required: true,
+    validate: {
+      validator: function(v) {
+        return /^\d{2,3}-\d+$/.test(v)
+      },
+      message: props => `${props.value} is not a valid phone number! The number must be in the format XX-XXXXXXX or XXX-XXXXXXX`
+    }
   }
 })
 
